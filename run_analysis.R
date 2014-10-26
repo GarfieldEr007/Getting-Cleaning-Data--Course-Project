b7048356a0ca3433a4b0630abea0a54d18f32de7
GarfieldEr007 <guoerzq@gmail.com>
2014-10-26 20:26:32 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,17 +6,17 @@ run_analysis <- function() {
 
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")    ## read training data
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")       ## read test data
-subject <- rbind(subject_train, subject_test)                             ## merge training and test data
-names(subject) <- "subject"                                               ## naming
+subject <- rbind(subject_train, subject_test)                             	## merge training and test data
+names(subject) <- "subject"                                               	## naming
 
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")                ## read y training data
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")                   ## read y  test data
-y <- rbind(y_train, y_test)                                               ## merge training and test data
-names(y) <- "activity"                                                    ## naming
+y <- rbind(y_train, y_test)                                               	## merge training and test data
+names(y) <- "activity"                                                    	## naming
 
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")                ## read x training data 
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")                   ## read x test data
-x <- rbind(x_train, x_test)                                               ## merge x training and test data
+x <- rbind(x_train, x_test)                                               	## merge x training and test data
 
 ## read 561 features and assign them to the names of x data
 features <- read.table("./UCI HAR Dataset/features.txt") 
@@ -52,28 +52,28 @@ activity_names <- c("Walking", "Walking_Up", "Walking_Down", "Sitting", "Standin
 tidy_data <- data.frame(NULL)
 x_same <- data.frame(NULL)
 
-for(s in 1:subject_length){           ## for1
-	for(a in activity_names){           ## for2
+for(s in 1:subject_length){           			## for1
+	for(a in activity_names){           		## for2
 
-		names <- c(s, a)                  ## subject and activity names
+		names <- c(s, a)                		## subject and activity names
 
-		for(i in 1:rownum){               ## for3
+		for(i in 1:rownum){               		## for3
 			name_temp <- c(data2[i, "subject"], data2[i, "activity"])
-			if(all(names == name_temp)){    ## if matching successfully, store the content of this row
+			if(all(names == name_temp)){    	## if matching successfully, store the content of this row
 			x_same <- rbind(x_same, data2[i, 3:colnum])
 			##next
-		}                                 ## if
-		}                                 ## for3
+		}                                 		## if
+		}                                 		## for3
 		
-		x_mean <- sapply(x_same, mean)    ## compute the average
+		x_mean <- sapply(x_same, mean)    		## compute the average
 		tidy_data <- rbind(tidy_data, c(names, x_mean))   ## add subject, activity and average into tidy data set
-		x_same <- data.frame(NULL)        ## assign NUL to it for next loop
+		x_same <- data.frame(NULL)        		## assign NUL to it for next loop
 
-		}                                 ## for2
+		}                                 		## for2
 	
-}                                     ## for1
+}                                     			## for1
 
-names(tidy_data) <- names(data2)      ## set the name of tidy data
+names(tidy_data) <- names(data2)      			## set the name of tidy data
 write.table(tidy_data, "tidy_data.txt", row.name = FALSE)   ## creating txt file and store the tidy data
 
 }
